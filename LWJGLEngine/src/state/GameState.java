374ba47432bc432f5c2e2112c4610c10d341b5cc
Andwerpz <andwerpooni@gmail.com>
2022-09-26 10:18:47 -0500

    dehost on exit to menu

--- LWJGLEngine/src/state/GameState.java
+++ LWJGLEngine/src/state/GameState.java
@@ -28,134 +28,156 @@ import util.Vec3;
 
 public class GameState extends State {
 
-    private static int WORLD_SCENE = 0;
+	private static int WORLD_SCENE = 0;
 
-    private String ip;
-    private int port;
+	private String ip;
+	private int port;
 
-    private boolean hosting;
+	private boolean hosting;
 
-    private static Screen perspectiveScreen;
-    private static Camera perspectiveCamera;
+	private static Screen perspectiveScreen;
+	private static Camera perspectiveCamera;
 
-    private Player player;
-
-    // -- NETWORKING --
-    private GameClient client;
-    private GameServer server;
-
-    private HashMap<Integer, Capsule> otherPlayers;
-
-    private long mapID;
-
-    public GameState(StateManager sm, String ip, int port, boolean hosting) {
-	super(sm);
-	this.ip = ip;
-	this.port = port;
-	this.hosting = hosting;
-
-	if (this.hosting) {
-	    this.ip = NetworkingUtils.getLocalIP();
-	}
-    }
-
-    @Override
-    public void load() {
-	if (perspectiveScreen == null) {
-	    perspectiveCamera = new Camera(Main.FOV, Main.windowWidth, Main.windowHeight, Main.NEAR, Main.FAR);
-	    perspectiveScreen = new PerspectiveScreen();
-	    perspectiveScreen.setCamera(perspectiveCamera);
-	}
-
-	Main.lockCursor();
-	InputManager.removeAllInputs();
-	Entity.killAll();
-
-	// -- WORLD SCENE --
-	Model.removeInstancesFromScene(WORLD_SCENE);
-	Light.removeLightsFromScene(WORLD_SCENE);
-	this.mapID = Model.addInstance(AssetManager.getModel("dust2"), Mat4.rotateX((float) Math.toRadians(90)).mul(Mat4.scale((float) 0.05)), WORLD_SCENE);
-	Model.activateCollisionMesh(this.mapID);
-	Light.addLight(WORLD_SCENE, new DirLight(new Vec3(0.3f, -1f, -0.5f), new Vec3(0.8f), 0.3f));
-	Scene.skyboxes.put(WORLD_SCENE, AssetManager.getSkybox("lake_skybox"));
-	player = new Player(new Vec3(18.417412f, 0.7f, -29.812654f), WORLD_SCENE);
+	private Player player;
 
 	// -- NETWORKING --
-	this.client = new GameClient();
-	this.otherPlayers = new HashMap<>();
+	private GameClient client;
+	private GameServer server;
 
-	if (this.hosting) {
-	    this.server = new GameServer(this.ip, this.port);
+	private HashMap<Integer, Capsule> otherPlayers;
+
+	private long mapID;
+
+	public GameState(StateManager sm, String ip, int port, boolean hosting) {
+		super(sm);
+		this.ip = ip;
+		this.port = port;
+		this.hosting = hosting;
+
+		if(this.hosting) {
+			this.ip = NetworkingUtils.getLocalIP();
+		}
 	}
 
-	for (int i = 0; i < 3; i++) {
-	    if (this.client.connect(this.ip, this.port)) {
-		break;
-	    }
+	@Override
+	public void load() {
+		if(perspectiveScreen == null) {
+			perspectiveCamera = new Camera(Main.FOV, Main.windowWidth, Main.windowHeight, Main.NEAR, Main.FAR);
+			perspectiveScreen = new PerspectiveScreen();
+			perspectiveScreen.setCamera(perspectiveCamera);
+		}
+
+		Main.lockCursor();
+		InputManager.removeAllInputs();
+		Entity.killAll();
+
+		// -- WORLD SCENE --
+		Model.removeInstancesFromScene(WORLD_SCENE);
+		Light.removeLightsFromScene(WORLD_SCENE);
+		this.mapID = Model.addInstance(AssetManager.getModel("dust2"),
+				Mat4.rotateX((float) Math.toRadians(90)).mul(Mat4.scale((float) 0.05)), WORLD_SCENE);
+		Model.activateCollisionMesh(this.mapID);
+		Light.addLight(WORLD_SCENE, new DirLight(new Vec3(0.3f, -1f, -0.5f), new Vec3(0.8f), 0.3f));
+		Scene.skyboxes.put(WORLD_SCENE, AssetManager.getSkybox("lake_skybox"));
+		player = new Player(new Vec3(18.417412f, 0.7f, -29.812654f), WORLD_SCENE);
+
+		// -- NETWORKING --
+		this.client = new GameClient();
+		this.otherPlayers = new HashMap<>();
+
+		if(this.hosting) {
+			this.startHosting();
+		}
+
+		for (int i = 0; i < 3; i++) {
+			if(this.connect()) {
+				break;
+			}
+		}
 	}
-    }
-
-    @Override
-    public void update() {
-	// -- NETWORKING --
-
-	// check for disconnected host
-	if (!this.client.isConnected()) {
-	    if (this.server != null && this.server.isRunning()) {
+	
+	private void startHosting() {
+		this.server = new GameServer(this.ip, this.port);
+	}
+	
+	private void stopHosting() {
+		if(this.server == null) {
+			return;
+		}
 		this.server.exit();
-	    }
-	    if (this.client.isRunning()) {
-		this.client.exit();
-	    }
-
-	    this.sm.switchState(new MainMenuState(this.sm));
+	}
+	
+	private boolean connect() {
+		return this.client.connect(this.ip, this.port);
+	}
+	
+	private void disconnect() {
+		this.client.disconnect();
 	}
 
-	// update player information
-	HashMap<Integer, Vec3> otherPlayerPositions = this.client.getOtherPlayerPositions();
-	for (int ID : otherPlayerPositions.keySet()) {
-	    Vec3 pos = otherPlayerPositions.get(ID);
-	    if (!this.otherPlayers.keySet().contains(ID)) {
-		this.otherPlayers.put(ID, new Capsule(pos, new Vec3(0), 0.33f, 1f, WORLD_SCENE));
-	    }
-	    Capsule c = this.otherPlayers.get(ID);
-	    c.setPos(pos);
-	    c.updateModelMats();
+	@Override
+	public void update() {
+		// -- NETWORKING --
+
+		// check for disconnected host
+		if(!this.client.isConnected()) {
+			if(this.server != null && this.server.isRunning()) {
+				this.server.exit();
+			}
+			if(this.client.isRunning()) {
+				this.client.exit();
+			}
+
+			this.sm.switchState(new MainMenuState(this.sm));
+		}
+
+		// update player information
+		HashMap<Integer, Vec3> otherPlayerPositions = this.client.getOtherPlayerPositions();
+		for (int ID : otherPlayerPositions.keySet()) {
+			Vec3 pos = otherPlayerPositions.get(ID);
+			if(!this.otherPlayers.keySet().contains(ID)) {
+				this.otherPlayers.put(ID, new Capsule(pos, new Vec3(0), 0.33f, 1f, WORLD_SCENE));
+			}
+			Capsule c = this.otherPlayers.get(ID);
+			c.setPos(pos);
+			c.updateModelMats();
+		}
+
+		ArrayList<Integer> disconnectedPlayers = this.client.getDisconnectedPlayers();
+		for (int ID : disconnectedPlayers) {
+			this.otherPlayers.get(ID).kill();
+			this.otherPlayers.remove(ID);
+		}
+
+		// -- UPDATES --
+		Entity.updateEntities();
+		Model.updateModels();
+		updateCamera();
+
+		this.client.setPos(this.player.pos);
+
+		// input
+		if(KeyboardInput.isKeyPressed(GLFW_KEY_M)) {
+			this.disconnect();
+			this.stopHosting();
+			this.sm.switchState(new MainMenuState(this.sm));
+		}
 	}
 
-	ArrayList<Integer> disconnectedPlayers = this.client.getDisconnectedPlayers();
-	for (int ID : disconnectedPlayers) {
-	    this.otherPlayers.get(ID).kill();
-	    this.otherPlayers.remove(ID);
+	@Override
+	public void render(Framebuffer outputBuffer) {
+		perspectiveScreen.render(outputBuffer, WORLD_SCENE);
 	}
 
-	// -- UPDATES --
-	Entity.updateEntities();
-	Model.updateModels();
-	updateCamera();
-
-	this.client.setPos(this.player.pos);
-
-	// input
-	if (KeyboardInput.isKeyPressed(GLFW_KEY_M)) {
-	    this.sm.switchState(new MainMenuState(this.sm));
+	private void updateCamera() {
+		perspectiveCamera.setFacing(player.camXRot, player.camYRot);
+		perspectiveCamera.setPos(player.pos.add(Player.cameraVec).sub(perspectiveCamera.getFacing().mul(0f)));
+		perspectiveCamera.setUp(new Vec3(0, 1, 0));
 	}
-    }
 
-    @Override
-    public void render(Framebuffer outputBuffer) {
-	perspectiveScreen.render(outputBuffer, WORLD_SCENE);
-    }
-
-    private void updateCamera() {
-	perspectiveCamera.setFacing(player.camXRot, player.camYRot);
-	perspectiveCamera.setPos(player.pos.add(Player.cameraVec).sub(perspectiveCamera.getFacing().mul(0f)));
-	perspectiveCamera.setUp(new Vec3(0, 1, 0));
-    }
-
-    @Override
-    public void mousePressed(int button) {
-	// shoot ray in direction of camera
+	@Override
+	public void mousePressed(int button) {
+		// shoot ray in direction of camera
 //		Vec3 ray_origin = perspectiveCamera.getPos();
 //		Vec3 ray_dir = perspectiveCamera.getFacing();
 //		ArrayList<Vec3> intersect = Model.rayIntersect(WORLD_SCENE, ray_origin, ray_dir);
@@ -178,12 +200,12 @@ public class GameState extends State {
 //		//Capsule c = new Capsule(cam_pos, cam_dir.mul(0.3f), 0.25f, 1f, WORLD_SCENE);
 //		Ball b = new Ball(cam_pos, cam_dir.mul(0.3f), 0.3f, WORLD_SCENE);
 //		System.out.println(cam_pos + " " + cam_dir.mul(0.6f));
-    }
+	}
 
-    @Override
-    public void mouseReleased(int button) {
-	// TODO Auto-generated method stub
+	@Override
+	public void mouseReleased(int button) {
+		// TODO Auto-generated method stub
 
-    }
+	}
 
 }
