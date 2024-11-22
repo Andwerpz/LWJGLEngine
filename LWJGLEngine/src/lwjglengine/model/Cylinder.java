e23bc9029c88d4ca8402f8faf0f560b027f70703
Andwerpz <andwerpooni@gmail.com>
2024-11-21 22:03:29 -0600

    finish cylinder

--- LWJGLEngine/src/lwjglengine/model/Cylinder.java
+++ LWJGLEngine/src/lwjglengine/model/Cylinder.java
@@ -4,6 +4,9 @@ import static org.lwjgl.opengl.GL11.*;
 
 import java.util.ArrayList;
 
+import myutils.math.Mat4;
+import myutils.math.MathUtils;
+import myutils.math.Quaternion;
 import myutils.math.Vec3;
 
 public class Cylinder extends Model {
@@ -64,9 +67,50 @@ public class Cylinder extends Model {
 		}
 
 		//top and bottom caps
+		int top_center_ind = indptr ++;
+		vertex_list.add(new Vec3(0, 0, 1));
+		int bot_center_ind = indptr ++;
+		vertex_list.add(new Vec3(0, 0, -1));
+		for (int i = 0; i < loop.length; i++) {
+			top_inds[i] = indptr++;
+			vertex_list.add(loop[i].add(new Vec3(0, 0, 1)));
+			bot_inds[i] = indptr++;
+			vertex_list.add(loop[i].add(new Vec3(0, 0, -1)));
+		}
+		for(int i = 0; i < loop.length; i++) {
+			ind_list.add(top_center_ind);
+			ind_list.add(top_inds[(i + 1) % loop.length]);
+			ind_list.add(top_inds[i]);
+			
+			ind_list.add(bot_center_ind);
+			ind_list.add(bot_inds[i]);
+			ind_list.add(bot_inds[(i + 1) % loop.length]);
+		}
 
 		VertexArray va = new VertexArray(vertex_list, ind_list, GL_TRIANGLES);
 		return va;
+	}
+	
+	public ModelInstance addCylinder(ModelTransform transform, int scene) {
+		return new ModelInstance(this, transform, scene);
+	}
+	
+	public static ModelTransform generateCylinderModelTransform(float length, float radius, Vec3 pos, Vec3 facing) {
+		Mat4 transform = Mat4.scale(new Vec3(radius, radius, length / 2.0));
+		transform.muli(Mat4.rotateAToB(new Vec3(0, 0, 1), facing));
+		transform.muli(Mat4.translate(pos));
+		return new ModelTransform(transform);
+	}
+	
+	public static ModelInstance addDefaultCylinder(float length, float radius, Vec3 pos, Vec3 facing, int scene) {
+		ModelTransform transform = generateCylinderModelTransform(length, radius, pos, facing);
+		return DEFAULT_CYLINDER.addCylinder(transform, scene);
+	}
+	
+	public static ModelInstance addDefaultCylinder(float length, float radius, Vec3 pos, Quaternion orient, int scene) {
+		Vec3 facing = MathUtils.quaternionRotateVec3(orient, new Vec3(0, 0, 1));
+		ModelTransform transform = generateCylinderModelTransform(length, radius, pos, facing);
+		return DEFAULT_CYLINDER.addCylinder(transform, scene);
 	}
 
 }
